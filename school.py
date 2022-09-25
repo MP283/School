929e02698bdd4b8889cdd8af725f7093986a0aff
MP283 <moizhakimpatra53@gmail.com>
2022-09-25 16:48:06 +0530

    Create school.py

---
+++ school.py
@@ -0,0 +1,61 @@
+# Made by Moiz patra
+
+# check wether its a prime or not
+######################################################
+# print("")
+# n = int(input("Enter a no."))
+
+# count = 0
+
+# for i in range(1, n, 1):
+#     if n%i==0:
+#         count = count + 1
+        
+        
+
+# if count <= 2:
+#     print(n," is a prime no.")
+# else:
+#         print(n," is not a prime no.")
+
+######################################################
+
+# Take input from user and find its sum
+######################################################
+# print("")
+# n = int(input("Enter num 1 :-"))
+# m = int(input("Enter num 2 :-"))
+
+# print("Sum of both the number is",n+m)
+
+######################################################
+
+# check if no is odd or even
+######################################################
+# print("")
+# n = int(input("input no."))
+# if n % 2 == 0:
+#     print("Number is even")
+# else:
+#     print("Number is odd")
+
+######################################################
+
+# print 1 to 10 using while loop
+###################################################### 
+# n = 1
+# while n <= 10:
+#     print(n)
+#     n = n + 1
+
+######################################################
+
+# print the table of no. user gave
+######################################################
+# n = int(input("Enter a no."))
+# i = 1
+# while i < 11:
+#     print(n, " X ", i, " = ", n*i)
+#     i = i + 1
+
+######################################################
